6b7ede89481c62c8da9c62979c494a3063d34c7e
Faiz Ahamed <76490386+FaizAhamed2o1@users.noreply.github.com>
2024-12-31 08:06:56 +0600

    register page added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
-const Login = () => {
-  const handleLogin = (e) => {
+const Register = () => {
+  const handleRegister = (e) => {
     e.preventDefault();
 
     console.log(e.currentTarget);
@@ -15,32 +15,59 @@ const Login = () => {
       <Navbar></Navbar>
 
       <h2 className="mt-10 mb-5 text-3xl font-semibold text-center">
-        Please Login
+        Please Register
       </h2>
 
       <div className="hero">
         <div className="hero-content lg:w-1/2 md:w-3/4 mx-auto">
           <div className="card bg-base-100 w-full shadow-2xl">
-            <form className="card-body" onSubmit={handleLogin}>
+            <form className="card-body" onSubmit={handleRegister}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="input input-bordered"
+                  name="name"
+                  required
+                />
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your photo URL"
+                  className="input input-bordered"
+                  name="name"
+                  required
+                />
+              </div>
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
-                  placeholder="Email"
+                  placeholder="Enter your email address"
                   className="input input-bordered"
                   name="email"
                   required
                 />
               </div>
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
                 <input
                   type="password"
-                  placeholder="Password"
+                  placeholder="Enter your password"
                   className="input input-bordered"
                   name="password"
                   required
@@ -51,14 +78,15 @@ const Login = () => {
                   </a>
                 </label>
               </div>
+
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
               </div>
 
               <p className="mt-7 text-center">
-                Don&apos;t have an account?{" "}
+                Already have an account?{" "}
                 <span className="text-secondary font-semibold">
-                  <Link to="/register">Register</Link>
+                  <Link to="/login">Register</Link>
                 </span>
               </p>
             </form>
@@ -69,4 +97,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
